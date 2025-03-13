cfa7bcbc0e29ea41ee9cb7afec162c89e5b192d8
dwarakaprasad <dwarka.damodaran@gmail.com>
2025-03-13 03:10:07 -0400

    Initial commit

---
+++ common-libs/types-clearance/src/main/java/com/arc/arp/App.java
@@ -0,0 +1,13 @@
+package com.arc.arp;
+
+/** Hello world! */
+public final class App {
+  // Private constructor to prevent instantiation
+  private App() {
+    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
