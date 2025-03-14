c7bda61a790182761c3c78308f44e04e8c440f16
dwarakaprasad <dwarka.damodaran@gmail.com>
2025-03-14 10:16:07 -0400

    Update code coverage to 95%

--- common-libs/types-clearance/src/main/java/com/arc/arp/App.java
+++ common-libs/types-clearance/src/main/java/com/arc/arp/App.java
@@ -1,13 +1,4 @@
 package com.arc.arp;
 
 /** Hello world! */
-public final class App {
-  // Private constructor to prevent instantiation
-  private App() {
-    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
-  }
-
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+public final class App {}
